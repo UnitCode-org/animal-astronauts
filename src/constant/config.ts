0aa08ee8688f20bf60f8ae9b627debfb424c9793
maxx2002 <max.wiranata@unitcode.org>
2024-08-23 11:09:09 +0700

    feat: update site config url

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: "Animal Astronauts",
   description:
     "Secure Your Animal Astronaut NFT built upon the Unit Network and Elevate Your Position in the Universe",
-  url: "https://www.animalastronauts.com",
+  url: "https://www.animalastronauts.org",
 };
